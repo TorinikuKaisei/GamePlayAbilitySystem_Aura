c449db08a0d3d75c65d4864a67fe21f8e7e8d55a
TorinikuNonkaisei <Toriatsu12@gmail.com>
2025-03-09 14:36:09 +0900

    ビルド時のエラー修正
    Build.csにUMGを追加

--- Source/Aura/Aura.Build.cs
+++ Source/Aura/Aura.Build.cs
@@ -8,7 +8,7 @@ public class Aura : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] {"EnhancedInput", "Core", "CoreUObject", "Engine", "InputCore", "GameplayAbilities", });
+		PublicDependencyModuleNames.AddRange(new string[] {"EnhancedInput", "Core", "CoreUObject", "Engine", "InputCore", "GameplayAbilities","UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayTags", "GameplayTasks" });
 
