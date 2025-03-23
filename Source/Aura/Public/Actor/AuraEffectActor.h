b1cb17b87cf4ed7accdead481aa904edc5ec4093
TorinikuNonkaisei <Toriatsu12@gmail.com>
2025-03-23 10:33:59 +0900

    Instant GameplayEffect

--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -22,7 +22,8 @@ protected:
 	virtual void BeginPlay() override;
 
 	UFUNCTION(BlueprintCallable)
-	void ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass);
-	UPROPERTY(EditAnywhere, Category="Applied Effects")
+	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Applied Effects")
 	TSubclassOf<UGameplayEffect>InstantGameplayEffectClass;
 };
