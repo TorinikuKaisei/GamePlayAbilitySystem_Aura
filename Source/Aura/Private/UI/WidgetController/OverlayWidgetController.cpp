6ef80a60ab2bd1a96436056f9f2e64cc433c9c82
TorinikuNonkaisei <Toriatsu12@gmail.com>
2025-04-06 18:20:55 +0900

    Broadcasting Data Table Rows

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -38,10 +38,15 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 		{
 			for (const FGameplayTag& Tag : AssetTags)
 			{
-				const FString Msg = FString::Printf(TEXT("GE Tag: %s"),*Tag.ToString());
-				GEngine->AddOnScreenDebugMessage(-1,8.f,FColor::Blue,Msg);
-
-				FUIWidgetRow* Row = GetDataTableRowByTag<FUIWidgetRow>(MessageWidgetDataTable,Tag);
+				//例；Tag = Message.Health Potion
+				// "Message.Health Potion".MatchesTag("Message") は True を返します。"Message".MatchesTag("Message.Health Potion") は False を返します。TagToCheckが無効な場合は常に False を返します
+				
+				FGameplayTag MessageTag = FGameplayTag::RequestGameplayTag(FName("Message"));
+				if (Tag.MatchesTag(MessageTag))
+				{
+					const FUIWidgetRow* Row = GetDataTableRowByTag<FUIWidgetRow>(MessageWidgetDataTable,Tag);
+					MessageWidgetRowDelegate.Broadcast(*Row);
+				}
 			}
 		}
 	);
