65df2329b41a1ad5f76e50678d473e74a412e3c6
TorinikuNonkaisei <Toriatsu12@gmail.com>
2025-04-30 09:47:00 +0900

    . Constructing the Attribute Menu Widget Controller

--- Source/Aura/Public/UI/HUD/AuraHUD.h
+++ Source/Aura/Public/UI/HUD/AuraHUD.h
@@ -7,6 +7,7 @@
 #include "UI/WidgetController/AuraWidgetController.h"
 #include "AuraHUD.generated.h"
 
+class UAttributeMenuWidgetController;
 class UOverlayWidgetController;
 class UAuraUserWidget;
 struct FWidgetControllerParams;
@@ -19,17 +20,20 @@ class AURA_API AAuraHUD : public AHUD
 	GENERATED_BODY()
 public:
 
-	UPROPERTY()
-	TObjectPtr<UAuraUserWidget> OverlayWidget;
+
 
 	UOverlayWidgetController* GetOverlayWidgetController(const FWidgetControllerParams& WCParams);
+	UAttributeMenuWidgetController* GetAttributeMenuWidgetController(const FWidgetControllerParams& WCParams);
 
 	void InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC, UAttributeSet* AS);
 protected:
 
 	
 private:
- 
+
+	UPROPERTY()
+ 	TObjectPtr<UAuraUserWidget> OverlayWidget;
+
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<UAuraUserWidget> OverlayWidgetClass;
 
@@ -38,4 +42,10 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<UOverlayWidgetController> OverlayWidgetControllerClass;
+
+	UPROPERTY()
+	TObjectPtr<UAttributeMenuWidgetController> AttributeMenuWidgetController;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UAttributeMenuWidgetController> AttributeMenuWidgetControllerClass;
 };
